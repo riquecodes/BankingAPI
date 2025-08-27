b468fbf93d0efaf04a31277021557a372a94e0fb
Henrique Riva <hbriva123@gmail.com>
2025-08-27 18:02:34 -0300

    chore: iniciando a configuração do Swagger para testes

--- UsersManagement/Program.cs
+++ UsersManagement/Program.cs
@@ -4,12 +4,11 @@ using UsersManagement.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddControllers();
 builder.Services.AddScoped<IUsersRepository, UserRepository>();
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -17,12 +16,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API v1");
+});
 
 app.UseHttpsRedirection();
 
