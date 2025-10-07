3881cbc9f9464f6bf4d14a9da155c6dff3506490
Henrique Riva <hbriva123@gmail.com>
2025-10-06 23:58:07 -0300

    feat: add a new AccountDTO

---
+++ BankingAPI/Models/AccountModelDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BankingAPI.Models
+{
+    public class AccountModelDTO
+    {
+        public decimal Balance { get; set; }
+        public required string AccountNumber { get; set; }
+        public AccountType AccountType { get; set; } = AccountType.Checking;
+        public string Agency { get; set; } = "0001";
+    }
+}
